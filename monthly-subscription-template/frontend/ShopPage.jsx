91278557cafc91c64f47e217ce5ee8193b5db23e
Antoine Charette <antoineocharette@gmail.com>
2023-11-10 11:23:03 -0500

    Changing the rendered billing page

--- monthly-subscription-template/frontend/ShopPage.jsx
+++ monthly-subscription-template/frontend/ShopPage.jsx
@@ -1,7 +1,7 @@
 import { useAction, useFindMany } from "@gadgetinc/react";
 import {
   Banner,
-  InlineStack,
+  BlockStack,
   Layout,
   Page,
   Spinner,
@@ -33,7 +33,11 @@ const ShopPage = () => {
     });
 
   const [
-    { fetching: fetchingSubscription, error: errorSubscribing },
+    {
+      data: subscription,
+      fetching: fetchingSubscription,
+      error: errorSubscribing,
+    },
     subscribe,
   ] = useAction(api.shopifyShop.subscribe, {
     select: {
@@ -88,39 +92,36 @@ const ShopPage = () => {
 
   return (
     <Page title="Plan Selection Page">
-      <Layout>
-        <Layout.Section>
-          {show && (
-            <Banner
-              title={bannerContext}
-              tone="critical"
-              onDismiss={handleDismiss}
-            />
-          )}
-          {shop?.plan?.id && (
-            <Banner
-              title={`This shop is assigned to the ${shop?.plan?.name} plan`}
-              tone="success"
-            >
-              {trialDays && (
-                <Text as="p" variant="bodyMd">
-                  You have <strong>{trialDays}</strong> trial days remaining.
-                </Text>
-              )}
-            </Banner>
-          )}
-        </Layout.Section>
-        <Layout.Section>
-          <InlineStack gap="400">
-            {!fetchingPlans ? (
-              plans.length ? (
-                plans?.map((plan) => (
+      <BlockStack gap="500">
+        {show && (
+          <Banner
+            title={bannerContext}
+            tone="critical"
+            onDismiss={handleDismiss}
+          />
+        )}
+        {shop?.plan?.id && (
+          <Banner
+            title={`This shop is assigned to the ${shop?.plan?.name} plan`}
+            tone="success"
+          >
+            {trialDays && (
+              <Text as="p" variant="bodyMd">
+                You have <strong>{trialDays}</strong> trial days remaining.
+              </Text>
+            )}
+          </Banner>
+        )}
+        <Layout>
+          {!fetchingPlans ? (
+            plans.length ? (
+              plans?.map((plan) => (
+                <Layout.Section variant="oneThird" key={plan.id}>
                   <PlanCard
-                    key={plan.id}
                     id={plan.id}
                     name={plan.name}
                     description={plan.description}
-                    monthlyPrice={prices[plan.id] || plan.monthlyPrice}
+                    monthlyPrice={prices[plan.id]}
                     trialDays={
                       calculateTrialDays(
                         shop?.usedTrialDays,
@@ -131,24 +132,27 @@ const ShopPage = () => {
                     }
                     currency={shop?.currency || "CAD"}
                     handleSubscribe={handleSubscribe}
-                    buttonDisabled={shop?.plan?.id === plan.id}
+                    buttonDisabled={
+                      subscription ||
+                      fetchingSubscription ||
+                      shop?.plan?.id === plan.id
+                    }
                   />
-                ))
-              ) : (
-                <Text as="p" variant="bodyLg">
-                  There are no plans in the database. Please make sure to add
-                  plans using the API Playground. Since the database is split
-                  between development and production, make sure to also add
-                  plans to your production database once deploying and going
-                  live.
-                </Text>
-              )
+                </Layout.Section>
+              ))
             ) : (
-              <Spinner />
-            )}
-          </InlineStack>
-        </Layout.Section>
-      </Layout>
+              <Text as="p" variant="bodyLg">
+                There are no plans in the database. Please make sure to add
+                plans using the API Playground. Since the database is split
+                between development and production, make sure to also add plans
+                to your production database once deploying and going live.
+              </Text>
+            )
+          ) : (
+            <Spinner />
+          )}
+        </Layout>
+      </BlockStack>
     </Page>
   );
 };
--- monthly-subscription-template/frontend/components/PlanCard.jsx
+++ monthly-subscription-template/frontend/components/PlanCard.jsx
@@ -5,6 +5,7 @@ import {
   BlockStack,
   Box,
   InlineStack,
+  SkeletonDisplayText,
 } from "@shopify/polaris";
 
 /**
@@ -21,30 +22,34 @@ export default ({
   currency,
   handleSubscribe,
   buttonDisabled,
-}) => {
-  return (
-    <Box maxWidth="465px">
-      <Card>
-        <BlockStack align="start" gap="500">
-          <InlineStack align="space-between">
-            <Text as="h2" variant="headingMd">
-              {name}
-            </Text>
+}) => (
+  <Box maxWidth="465px">
+    <Card>
+      <BlockStack align="start" gap="500">
+        <InlineStack align="space-between">
+          <Text as="h2" variant="headingMd">
+            {name}
+          </Text>
+          {monthlyPrice ? (
             <Text as="span" variant="bodyLg" fontWeight="medium">
               {monthlyPrice} {currency}
             </Text>
-          </InlineStack>
-          <Text as="p" variant="bodyMd">
-            {description}
-          </Text>
-          <Text as="p" variant="bodyMd" fontWeight="medium">
-            {trialDays} trial days
-          </Text>
-          <Button disabled={buttonDisabled} onClick={() => handleSubscribe(id)}>
-            Select
-          </Button>
-        </BlockStack>
-      </Card>
-    </Box>
-  );
-};
+          ) : (
+            <Box width="40%">
+              <SkeletonDisplayText size="small" />
+            </Box>
+          )}
+        </InlineStack>
+        <Text as="p" variant="bodyMd">
+          {description}
+        </Text>
+        <Text as="p" variant="bodyMd" fontWeight="medium">
+          {trialDays} trial days
+        </Text>
+        <Button disabled={buttonDisabled} onClick={() => handleSubscribe(id)}>
+          Select
+        </Button>
+      </BlockStack>
+    </Card>
+  </Box>
+);

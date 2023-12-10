ff81ddacce03dea8e28a11fe166e63772102f781
Antoine Charette <antoineocharette@gmail.com>
2023-12-10 16:41:31 -0500

    Updated framework version, gadget provided packages, polaris version, added abort action to shopifySync and removed unused imports and code

---
+++ one-time-charge-template/frontend/components/index.js
@@ -0,0 +1,6 @@
+/**
+ * @module ReactComponentIndex Index for all React components used in the apps frontend
+ * @exports (StyledSpinner) A component for rendering a styled spinner
+ */
+
+export { default as StyledSpinner } from "./StyledSpinner";

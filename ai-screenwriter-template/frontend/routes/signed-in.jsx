33b38c57f0c9cf40b7064cc0677b4ab3b0d4bec6
rdraward <riley@gadget.dev>
2023-12-15 09:12:36 -0500

    PR feedback: remove redundant required, isDirty flag for form reset, inline onClick

--- ai-screenwriter-template/frontend/routes/signed-in.jsx
+++ ai-screenwriter-template/frontend/routes/signed-in.jsx
@@ -1,53 +1,68 @@
-import { useGlobalAction, useFetch, useMaybeFindFirst, useActionForm } from "@gadgetinc/react";
+import {
+  useGlobalAction,
+  useFetch,
+  useMaybeFindFirst,
+  useActionForm,
+} from "@gadgetinc/react";
 import { api } from "../api";
 import { useCallback, useState } from "react";
 
 export default function () {
   // see if movie data exists in the database!
-  const [{ data: movieDataIngested, fetching: checkingIfMovieDataExists, error: errorCheckingForData }, retry] = useMaybeFindFirst(api.movie);
+  const [
+    {
+      data: movieDataIngested,
+      fetching: checkingIfMovieDataExists,
+      error: errorCheckingForData,
+    },
+    retry,
+  ] = useMaybeFindFirst(api.movie);
   // fire action used to ingest data from HuggingFace model
-  const [{ fetching: ingestingData, error: errorIngestingData }, ingestData] = useGlobalAction(api.ingestData);
+  const [{ fetching: ingestingData, error: errorIngestingData }, ingestData] =
+    useGlobalAction(api.ingestData);
 
   return (
     <main style={{ width: "60vw", position: "absolute", top: 40 }}>
       <div>
         <h1>Make-a-movie scene (with AI)</h1>
-        <p>Write a fake movie quote, pick a suggested movie, and let OpenAI generate a new scene!</p>
+        <p>
+          Write a fake movie quote, pick a suggested movie, and let OpenAI
+          generate a new scene!
+        </p>
       </div>
 
       <br />
 
-      {checkingIfMovieDataExists &&
-        <div className="loader"></div>
-      }
+      {checkingIfMovieDataExists && <div className="loader"></div>}
 
-      {ingestingData &&
+      {ingestingData && (
         <div className="row">
           <h2>Fetching movie data...</h2>
         </div>
-      }
+      )}
 
-      {errorCheckingForData &&
+      {errorCheckingForData && (
         <p className="error">{errorCheckingForData.message}</p>
-      }
+      )}
 
-      {errorIngestingData &&
+      {errorIngestingData && (
         <p className="error">{errorIngestingData.message}</p>
-      }
+      )}
 
-      {!movieDataIngested && !checkingIfMovieDataExists &&
-        <button onClick={async () => {
-          await ingestData();
-          await retry();
-        }
-        } disabled={ingestingData}>Ingest data</button>
-      }
+      {!movieDataIngested && !checkingIfMovieDataExists && (
+        <button
+          onClick={async () => {
+            await ingestData();
+            await retry();
+          }}
+          disabled={ingestingData}
+        >
+          Ingest data
+        </button>
+      )}
 
-      {
-        movieDataIngested && <MovieQuoteForm />
-      }
-
-    </main >
+      {movieDataIngested && <MovieQuoteForm />}
+    </main>
   );
 }
 
@@ -55,55 +70,76 @@ const MovieQuoteForm = () => {
   // state for the currently selected movie
   const [movie, setMovie] = useState("");
   // action for finding movies with similar quotes
-  const { submit, register, actionData, error, formState, watch, reset } = useActionForm(api.findSimilarMovies);
-
-  const pickMovie = useCallback((event) => {
-    setMovie(event.target.value);
-  }, []);
+  const { submit, register, actionData, error, formState, watch, reset } =
+    useActionForm(api.findSimilarMovies);
 
   // watch changes to the quote state in our form, and store in a variable
-  const quote = watch("quote")
+  const quote = watch("quote");
 
   return (
     <>
       <form onSubmit={submit} style={{ maxWidth: "100%" }}>
-        <div className="row" style={{ display: "flex", }}>
-          <input style={{ flex: "1", marginRight: "4px" }} required placeholder="Enter a fake movie quote!" {...register("quote")} disabled={formState.isSubmitting} />
-          <button type="submit" disabled={formState.isSubmitting} style={{ marginRight: "4px" }}>Find quotes</button>
-          <button disabled={!formState.isSubmitted} onClick={() => {
-            // reset the form
-            reset();
-          }
-          }>Reset</button>
+        <div className="row" style={{ display: "flex" }}>
+          <input
+            style={{ flex: "1", marginRight: "4px" }}
+            placeholder="Enter a fake movie quote!"
+            {...register("quote")}
+            disabled={formState.isSubmitting}
+          />
+          <button
+            type="submit"
+            disabled={formState.isSubmitting}
+            style={{ marginRight: "4px" }}
+          >
+            Find quotes
+          </button>
+          <button
+            disabled={!formState.isDirty}
+            onClick={() => {
+              // reset the form
+              reset();
+            }}
+          >
+            Reset
+          </button>
         </div>
       </form>
 
-      {error && <p className="error">There was an error fetching similar movies. Check your Gadget logs for more details!</p>}
+      {error && (
+        <p className="error">
+          There was an error fetching similar movies. Check your Gadget logs for
+          more details!
+        </p>
+      )}
       {actionData?.fetching && <div className="loader"></div>}
 
-      {
-        quote && actionData && (
-          <>
-            <div className="row"><p>Movies with similar quotes:</p></div>
-            <form>
-              <div className="row" style={{ display: "flex", flexWrap: "wrap" }}>
-                {actionData.map((movieRecord, i) => (
-                  <span key={`movie_option_${i}`} style={{ width: "50%" }}>
-                    <input type="radio" checked={movieRecord.title == movie} value={movieRecord.title} onChange={pickMovie} id={movieRecord.id} />
-                    <label htmlFor={movieRecord.id}>{movieRecord.title}</label>
-                  </span>
-                ))}
-              </div>
-            </form>
-          </>
-        )
-      }
-      {
-        movie && quote && <SceneGenerator movie={movie} quote={quote} />
-      }
+      {quote && actionData && (
+        <>
+          <div className="row">
+            <p>Movies with similar quotes:</p>
+          </div>
+          <form>
+            <div className="row" style={{ display: "flex", flexWrap: "wrap" }}>
+              {actionData.map((movieRecord, i) => (
+                <span key={`movie_option_${i}`} style={{ width: "50%" }}>
+                  <input
+                    type="radio"
+                    checked={movieRecord.title == movie}
+                    value={movieRecord.title}
+                    onChange={(e) => setMovie(e.target.value)}
+                    id={movieRecord.id}
+                  />
+                  <label htmlFor={movieRecord.id}>{movieRecord.title}</label>
+                </span>
+              ))}
+            </div>
+          </form>
+        </>
+      )}
+      {movie && quote && <SceneGenerator movie={movie} quote={quote} />}
     </>
   );
-}
+};
 
 const SceneGenerator = ({ movie, quote }) => {
   // call HTTP route and stream response from OpenAI
@@ -113,7 +149,7 @@ const SceneGenerator = ({ movie, quote }) => {
     headers: {
       "content-type": "application/json",
     },
-    stream: "string"
+    stream: "string",
   });
 
   return (
@@ -121,7 +157,20 @@ const SceneGenerator = ({ movie, quote }) => {
       <button onClick={() => void sendPrompt()}>Generate scene</button>
       {error && <p className="error">{error.message}</p>}
       {fetching && <div className="loader" />}
-      {data && <pre style={{ border: "dashed black 1px", background: "#f5f5f5", padding: "10px", maxHeight: "45vh", overflowY: "scroll", whiteSpace: "pre-wrap" }}>{data}</pre>}
+      {data && (
+        <pre
+          style={{
+            border: "dashed black 1px",
+            background: "#f5f5f5",
+            padding: "10px",
+            maxHeight: "45vh",
+            overflowY: "scroll",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {data}
+        </pre>
+      )}
     </section>
   );
-}
+};

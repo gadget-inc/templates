3cf20238bf02fc4fa91e8d9dec81c3b4f85adaa3
Antoine Charette <antoineocharette@gmail.com>
2024-04-12 12:01:11 -0400

    Fixing github actions code

---
+++ one-time-charge-template/accessControl/permissions.gadget.ts
@@ -0,0 +1,19 @@
+import type { GadgetPermissions } from "gadget-server";
+
+/**
+ * This metadata describes the access control configuration available in your application.
+ * Grants that are not defined here are set to false by default.
+ *
+ * View and edit your roles and permissions in the Gadget editor at https://one-time-charge-template.gadget.app/edit/settings/permissions
+ */
+export const permissions: GadgetPermissions = {
+  type: "gadget/permissions/v1",
+  roles: {
+    "shopify-app-users": {
+      storageKey: "Role-Shopify-App",
+    },
+    unauthenticated: {
+      storageKey: "unauthenticated",
+    },
+  },
+};

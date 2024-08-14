0ff273a3b8c181b929b942232a87037595da842a
Antoine Charette <antoineocharette@gmail.com>
2024-08-14 16:35:07 -0400

    Removing the polaris provider from the application

--- blog-template/web/main.jsx
+++ blog-template/web/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components";
 import { ChakraProvider } from "@chakra-ui/react";
-import { AppProvider } from "@shopify/polaris";
-import enTranslations from "@shopify/polaris/locales/en.json";
-import "@shopify/polaris/build/esm/styles.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("#root element not found for booting react app");
@@ -12,9 +9,7 @@ if (!root) throw new Error("#root element not found for booting react app");
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <ChakraProvider>
-      <AppProvider i18n={enTranslations}>
-        <App />
-      </AppProvider>
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );

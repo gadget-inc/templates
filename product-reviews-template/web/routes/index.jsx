6ac2e723cf5b4af4977f516a1321a26cbe893b33
Antoine Charette <antoineocharette@gmail.com>
2024-08-26 16:43:05 -0400

    Working on adding a nice table for approving and removing reviews

--- product-reviews-template/web/routes/index.jsx
+++ product-reviews-template/web/routes/index.jsx
@@ -13,55 +13,29 @@ import { api } from "../api";
 
 export default function () {
   return (
-    <Page title="App">
+    <Page>
       <Layout>
         <Layout.Section>
-          <Banner tone="success">
-            <Text variant="bodyMd" as="p">
-              Successfully connected your Gadget app to Shopify
-            </Text>
-          </Banner>
-        </Layout.Section>
-        <Layout.Section>
-          <Card>
-            <img
-              className="gadgetLogo"
-              src="https://assets.gadget.dev/assets/icon.svg"
-            />
-            <BlockStack gap="200">
-              <Text variant="headingMd" as="h1" alignment="center">
-                Edit this page:{" "}
-                <Link
-                  url={`/edit/${window.gadgetConfig.env.GADGET_ENV}/files/web/routes/index.jsx`}
-                  target="_blank"
-                  removeUnderline
-                >
-                  web/routes/index.jsx
-                </Link>
-              </Text>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <Card padding="0">
-            {/* use Autocomponents to build UI quickly: https://docs.gadget.dev/guides/frontend/autocomponents  */}
-            <AutoTable
-              model={api.shopifyShop}
-              columns={["name", "countryName", "currency", "customerEmail"]}
-            />
-            <Box padding="400">
-              <Text variant="headingMd" as="h6">
-                Shop records fetched from:{" "}
-                <Link
-                  url={`/edit/${window.gadgetConfig.env.GADGET_ENV}/model/DataModel-Shopify-Shop/data`}
-                  target="_blank"
-                  removeUnderline
-                >
-                  api/models/shopifyShop/data
-                </Link>
-              </Text>
-            </Box>
-          </Card>
+          <AutoTable
+            model={api.review}
+            select={{
+              rating: true,
+              anonymous: true,
+              approved: true,
+              product: {
+                title: true,
+              },
+              customer: { firstName: true, lastName: true },
+            }}
+            excludeColumns={[
+              "id",
+              "anonymous",
+              "metaobjectId",
+              "createdAt",
+              "updatedAt",
+            ]}
+            selectable={false}
+          />
         </Layout.Section>
       </Layout>
     </Page>

d404392f28ad24ecada46e7250a9140d40c56422
Antoine Charette <antoineocharette@gmail.com>
2024-08-26 09:28:22 -0400

    Changed the name of the folder to make more sense

--- standalone-shopify-template/accessControl/permissions.gadget.ts
+++ standalone-shopify-template/accessControl/permissions.gadget.ts
@@ -4,48 +4,11 @@ import type { GadgetPermissions } from "gadget-server";
  * This metadata describes the access control configuration available in your application.
  * Grants that are not defined here are set to false by default.
  *
- * View and edit your roles and permissions in the Gadget editor at https://external-shopify-template.gadget.app/edit/settings/permissions
+ * View and edit your roles and permissions in the Gadget editor at https://standalone-shopify-template.gadget.app/edit/settings/permissions
  */
 export const permissions: GadgetPermissions = {
   type: "gadget/permissions/v1",
   roles: {
-    "shopify-app-users": {
-      storageKey: "Role-Shopify-App",
-      models: {
-        shopifyGdprRequest: {
-          read: {
-            filter:
-              "accessControl/filters/shopify/shopifyGdprRequest.gelly",
-          },
-          actions: {
-            create: true,
-            update: true,
-          },
-        },
-        shopifyShop: {
-          read: {
-            filter: "accessControl/filters/shopify/shopifyShop.gelly",
-          },
-          actions: {
-            install: true,
-            reinstall: true,
-            uninstall: true,
-            update: true,
-          },
-        },
-        shopifySync: {
-          read: {
-            filter: "accessControl/filters/shopify/shopifySync.gelly",
-          },
-          actions: {
-            abort: true,
-            complete: true,
-            error: true,
-            run: true,
-          },
-        },
-      },
-    },
     "signed-in": {
       storageKey: "signed-in",
       default: {
@@ -86,6 +49,43 @@ export const permissions: GadgetPermissions = {
       },
       actions: {
         verifyConnections: true,
+      },
+    },
+    "shopify-app-users": {
+      storageKey: "Role-Shopify-App",
+      models: {
+        shopifyGdprRequest: {
+          read: {
+            filter:
+              "accessControl/filters/shopify/shopifyGdprRequest.gelly",
+          },
+          actions: {
+            create: true,
+            update: true,
+          },
+        },
+        shopifyShop: {
+          read: {
+            filter: "accessControl/filters/shopify/shopifyShop.gelly",
+          },
+          actions: {
+            install: true,
+            reinstall: true,
+            uninstall: true,
+            update: true,
+          },
+        },
+        shopifySync: {
+          read: {
+            filter: "accessControl/filters/shopify/shopifySync.gelly",
+          },
+          actions: {
+            abort: true,
+            complete: true,
+            error: true,
+            run: true,
+          },
+        },
       },
     },
     unauthenticated: {
--- standalone-shopify-template/api/models/shopPermission/schema.gadget.ts
+++ standalone-shopify-template/api/models/shopPermission/schema.gadget.ts
@@ -1,6 +1,6 @@
 import type { GadgetModel } from "gadget-server";
 
-// This file describes the schema for the "shopPermission" model, go to https://external-shopify-template.gadget.app/edit to view/edit your model in Gadget
+// This file describes the schema for the "shopPermission" model, go to https://standalone-shopify-template.gadget.app/edit to view/edit your model in Gadget
 // For more information on how to update this file http://docs.gadget.dev
 
 export const schema: GadgetModel = {

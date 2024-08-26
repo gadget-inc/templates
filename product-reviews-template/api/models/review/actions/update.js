ecf8951cf17e09eae04828a374883ac93b296292
Antoine Charette <antoineocharette@gmail.com>
2024-08-26 15:54:26 -0400

    Adding metaobject creation and deletion code in the update action of review

--- product-reviews-template/api/models/review/actions/update.js
+++ product-reviews-template/api/models/review/actions/update.js
@@ -11,15 +11,93 @@ import {
 export async function run({ params, record, logger, api, connections }) {
   applyParams(params, record);
 
-  const approved = record.changes("approved");
+  const { changed, current: approved } = record.changes("approved");
 
-  if (approved.changed) {
+  if (changed) {
     const shopify = connections.shopify.current;
 
-    if (approved.current && !record.metaobjectId) {
-      await shopify.graphql({});
-    } else if (!approved.current && record.metaobjectId) {
-      await shopify.graphql({});
+    if (approved && !record.metaobjectId) {
+      const customer = await api.shopifyCustomer.maybeFindOne(
+        record.customerId,
+        {
+          select: {
+            firstName: true,
+          },
+        }
+      );
+
+      // Create the metaobject
+      const metaobjectCreateResponse = await shopify.graphql(
+        `mutation ($metaobject: MetaobjectCreateInput!) {
+          metaobjectCreate(metaobject: $metaobject) {
+            metaobject {
+              id
+            }
+            userErrors {
+              message
+            }
+          }
+        }`,
+        {
+          metaobject: {
+            type: "review",
+            fields: [
+              {
+                key: "anonymous",
+                value:
+                  record.anonymous == true
+                    ? record.anonymous.toString()
+                    : customer?.firstName
+                      ? "true"
+                      : "false",
+              },
+              {
+                key: "rating",
+                value: record.rating.toString(),
+              },
+              {
+                key: "content",
+                value: record.content,
+              },
+              {
+                key: "product",
+                value: `gid://shopify/Product/${record.productId}`,
+              },
+            ],
+          },
+        }
+      );
+
+      // Throw an error if Shopify returns an error
+      if (metaobjectCreateResponse?.metaobjectCreate?.userErrors?.length)
+        throw new Error(
+          metaobjectCreateResponse.metaobjectCreate.userErrors[0].message
+        );
+
+      // Set the metaobject id on the record
+      record.metaobjectId =
+        metaobjectCreateResponse.metaobjectCreate.metaobject.id;
+    } else if (!approved && record.metaobjectId) {
+      // Delete the metaobject
+      const metaobjectDeleteResponse = await shopify.graphql(
+        `mutation ($id: ID!) {
+          metaobjectDelete(id: $id) {
+            deletedId
+            userErrors {
+              message
+            }
+          }
+        }`,
+        {
+          id: record.metaobjectId,
+        }
+      );
+
+      // Throw an error if Shopify returns an error
+      if (metaobjectDeleteResponse?.metaobjectDelete?.userErrors?.length)
+        throw new Error(
+          metaobjectDeleteResponse.metaobjectDelete.userErrors[0].message
+        );
     }
   }
 

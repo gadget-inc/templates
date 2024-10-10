a0df0e4201c9361299724fbf6b2459716615e47d
Antoine Charette <antoineocharette@gmail.com>
2024-10-10 13:52:03 -0400

    Upgrading framework version for the customized-bundle template

--- shopify/customized-bundle-template/settings.gadget.ts
+++ shopify/customized-bundle-template/settings.gadget.ts
@@ -2,7 +2,7 @@ import type { GadgetSettings } from "gadget-server";
 
 export const settings: GadgetSettings = {
   type: "gadget/settings/v1",
-  frameworkVersion: "v1.1.0",
+  frameworkVersion: "v1.3.0",
   plugins: {
     connections: {
       shopify: {

653138edf04bac7116bd0d33365ef826428c73b4
Antoine Charette <antoineocharette@gmail.com>
2024-10-10 14:00:47 -0400

    Upgrading the monthly-sub template

--- shopify/monthly-subscription-template/vite.config.mjs
+++ shopify/monthly-subscription-template/vite.config.mjs
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { gadget } from "gadget-server/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [gadget(), react()],
   clearScreen: false,
 });

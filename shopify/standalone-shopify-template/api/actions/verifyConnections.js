153f38203442c0d0b9f0ef8205cc2c9c37cf0950
Antoine Charette <antoineocharette@gmail.com>
2024-10-18 12:26:36 -0400

    Fiing issues from the PR

--- shopify/standalone-shopify-template/api/actions/verifyConnections.js
+++ shopify/standalone-shopify-template/api/actions/verifyConnections.js
@@ -21,7 +21,7 @@ export async function run({ params, logger, api, connections, session }) {
       userId: {
         equals: user,
       },
-      shop: {
+      shopId: {
         equals: shop,
       },
     },

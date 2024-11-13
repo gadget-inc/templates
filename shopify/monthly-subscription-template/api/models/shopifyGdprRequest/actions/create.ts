d7b9c3a6b9d704709b833ca56ff8b23ed1d25638
Antoine Charette <antoineocharette@gmail.com>
2024-11-13 16:25:33 -0500

    Fixing issues for esbuild

--- shopify/monthly-subscription-template/api/models/shopifyGdprRequest/actions/create.ts
+++ shopify/monthly-subscription-template/api/models/shopifyGdprRequest/actions/create.ts
@@ -25,19 +25,22 @@ export const onSuccess: ActionOnSuccess = async ({
   connections,
 }) => {
   switch (record.topic) {
-    case "customers/data_request":
+    case "customers/data_request": {
       // This process is a manual one. You must provide the customer's data to the store owners directly.
       // See https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#customers-data_request for more information.
       break;
-    case "customers/redact":
+    }
+    case "customers/redact": {
       // Any modifications that are initiated by Shopify and emitted via webhook will automatically be handled by your existing model actions.
       // The responsibility falls on you to redact any additional customer related data you may have in custom models.
       break;
-    case "shop/redact":
+    }
+    case "shop/redact": {
       // This will be received 48 hours after a store owner uninstalls your app. Any modifications that are initiated by Shopify and emitted via webhook will automatically be handled by your existing model actions.
       // The responsibility falls on you to redact any additional shop related data you may have in custom models.
       // See https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#shop-redact for more information.
       break;
+    }
   }
 };
 

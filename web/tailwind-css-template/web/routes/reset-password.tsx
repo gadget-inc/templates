6b235b528abd85900d0186c7868ea8d2c09b5aee
Antoine Charette <antoineocharette@gmail.com>
2024-11-13 16:07:07 -0500

    Fixing app load issue

--- web/tailwind-css-template/web/routes/reset-password.tsx
+++ web/tailwind-css-template/web/routes/reset-password.tsx
@@ -11,30 +11,49 @@ export default function () {
     watch,
     formState: { errors, isSubmitSuccessful, isSubmitting },
   } = useActionForm(api.user.resetPassword, {
-    defaultValues: { code: params.get("code") },
+    defaultValues: {
+      code: params.get("code"),
+      password: "",
+      confirmPassword: "",
+    },
   });
   const { configuration } = useAuth();
 
   return isSubmitSuccessful ? (
-    <p className="first-letter:capitalize text-[green]">
-      Password reset successfully. <Link to={configuration.signInPath}>Sign in now</Link>
+    <p className="format-message success">
+      Password reset successfully.{" "}
+      <Link to={configuration.signInPath}>Sign in now</Link>
     </p>
   ) : (
-    <form className="flex gap-3 flex-col max-w-[350px] w-full" onSubmit={submit}>
-      <h1 className="text-2xl font-semibold mb-0 pb-2">Reset password</h1>
-      <input className="text-base py-1 px-3 border border-[#CCC] border-solid rounded w-full" placeholder="New password" type="password" {...register("password")} />
-      {errors?.user?.password?.message && <p className="first-letter:capitalize text-[red]">{errors?.user?.password?.message}</p>}
+    <form className="custom-form" onSubmit={submit}>
+      <h1 className="form-title">Reset password</h1>
       <input
-        className="text-base py-1 px-3 border border-[#CCC] border-solid rounded w-full"
+        className="custom-input"
+        placeholder="New password"
+        type="password"
+        {...register("password")}
+      />
+      {errors?.user?.password?.message && (
+        <p className="format-message error">
+          {errors?.user?.password?.message}
+        </p>
+      )}
+      <input
+        className="custom-input"
         placeholder="Confirm password"
         type="password"
         {...register("confirmPassword", {
-          validate: (value) => value === watch("password") || "The passwords do not match",
+          validate: (value) =>
+            value === watch("password") || "The passwords do not match",
         })}
       />
-      {errors?.confirmPassword?.message && <p className="first-letter:capitalize text-[red]">{errors.confirmPassword.message}</p>}
-      {errors?.root?.message && <p className="first-letter:capitalize text-[red]">{errors.root.message}</p>}
-      <button disabled={isSubmitting} type="submit" className="py-0.5 border border-solid border-black rounded bg-[#efefef] hover:bg-[#e5e5e5]">
+      {errors?.confirmPassword?.message && (
+        <p className="format-message error">{errors.confirmPassword.message}</p>
+      )}
+      {errors?.root?.message && (
+        <p className="format-message error">{errors.root.message}</p>
+      )}
+      <button disabled={isSubmitting} type="submit">
         Reset password
       </button>
     </form>

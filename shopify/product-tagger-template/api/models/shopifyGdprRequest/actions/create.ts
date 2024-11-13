873814a988377c8d1556b8eba2abc3e35ad613ae
Antoine Charette <antoineocharette@gmail.com>
2024-11-13 16:34:58 -0500

    Fixing issues for esbuild

--- shopify/product-tagger-template/api/models/shopifyGdprRequest/actions/create.ts
+++ shopify/product-tagger-template/api/models/shopifyGdprRequest/actions/create.ts
@@ -18,18 +18,21 @@ export const onSuccess: ActionOnSuccess = async ({
   api,
 }) => {
   switch (record.topic) {
-    case "customers/data_request":
+    case "customers/data_request": {
       // This process is a manual one. You must provide the customer's data to the store owners directly.
       // See https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#customers-data_request for more information.
       break;
-    case "customers/redact":
+    }
+    case "customers/redact": {
       // Your Gadget Shopify connection will automatically delete all customer data in the Shopify models.
       // Use this block to redact any additional customer related data you may have kept in separate models.
       break;
-    case "shop/redact":
+    }
+    case "shop/redact": {
       // This will be received 48 hours after a store owner uninstalls your app.
       // See https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#shop-redact for more information.
       break;
+    }
   }
 };
 

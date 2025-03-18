fbfdd7a94759a89e2ae38f3acbf94cbb76d8e7ab
Antoine Charette <antoineocharette@gmail.com>
2025-03-18 17:47:17 -0400

    Removed old code // Added new styling

--- web/stripe-template/web/components/ProductCard.tsx
+++ web/stripe-template/web/components/ProductCard.tsx
@@ -1,96 +1,91 @@
-import { useGlobalAction } from "@gadgetinc/react";
-import { SyntheticEvent, useCallback, useContext } from "react";
+import { useMemo } from "react";
 import { api } from "../api";
-import type { Product } from "../backups/billing";
-import { UserContext } from "../providers";
-import { useNavigate } from "react-router-dom";
+import type { Product } from "../routes/_user.billing";
+import { useNavigate, useOutletContext } from "react-router";
+import { AuthOutletContext } from "@/routes/_user";
+import { AutoButton } from "./auto";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+type ProductCardProps = {
+  product: Product;
+  interval: string;
+};
 
 const ProductCard = ({
   product: { prices, name },
   interval,
-}: {
-  product: Product;
-  interval: string;
-}) => {
-  const { user } = useContext(UserContext);
+}: ProductCardProps) => {
+  const { user } = useOutletContext<AuthOutletContext>();
   const navigate = useNavigate();
 
-  const [{ data: stripeCheckoutUrl }, stripeSubscribe] = useGlobalAction(
-    api.createCheckoutSession
+  const updateEval = useMemo(
+    () =>
+      user?.stripeSubscription?.status == "active" && user?.stripeCustomerId,
+    [user]
   );
-
-  const [, updateSubscription] = useGlobalAction(api.updateSubscription);
-
-  const submit = useCallback(
-    async (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
-      e.preventDefault();
-      const form = e.target as HTMLFormElement;
-      const priceId = (form[0] as HTMLInputElement).value;
-
-      if (
-        user?.stripeSubscription?.status == "active" &&
-        user?.stripeCustomerId
-      ) {
-        const { data, error: subUpdateError } = await updateSubscription({
-          subscriptionId: user.stripeSubscription.stripeId,
-          priceId,
-        });
-
-        if (subUpdateError) console.error(subUpdateError);
-
-        if (data?.status === "ok") navigate("/");
-      } else {
-        // call the createCheckoutSession global action
-        void stripeSubscribe({ priceId });
-      }
-    },
-    []
-  );
-
-  // redirect to Stripe's checkout page
-  if (stripeCheckoutUrl) {
-    window.location.href = stripeCheckoutUrl;
-  }
 
   return (
-    <div className="pricing-card-container">
+    <Card className="flex flex-col flex-1 max-w-sm rounded-lg border border-gray-200 shadow-lg bg-white p-6 transition-all hover:shadow-xl">
       {prices.map((price, i) => {
-        if (price.interval === interval) {
+        if (price.interval === interval)
           return (
-            <div key={i}>
-              <div className="pricing-card">
-                <div className="card-header">
-                  <h2 className="plan-title">{name}</h2>
-                  <p className="plan-price">
-                    ${price.unitAmount / 100} / {price.interval}
-                  </p>
-                </div>
-                <div className="card-body">
-                  <ul className="features-list">
-                    <li>Feature 1</li>
-                    <li>Feature 2</li>
-                    <li>Feature 3</li>
-                  </ul>
-                </div>
-                <div className="card-footer">
-                  <form onSubmit={submit}>
-                    {/* Add the updateSubscription global action as an option if theres a current sub */}
-                    <input type="hidden" name="lookup_key" value={price.id} />
-                    <button
-                      className="btn-stripe-subscribe"
-                      type="submit"
-                      disabled={price.current}
-                    >
-                      {price.current ? "Current" : "Select"}
-                    </button>
-                  </form>
-                </div>
-              </div>
+            <div key={i} className="space-y-4">
+              <CardHeader className="flex flex-row items-center justify-between">
+                <CardTitle className="text-lg font-semibold text-gray-900">
+                  {name}
+                </CardTitle>
+                <CardDescription className="text-gray-600 text-sm">
+                  <span className="text-xl font-bold text-gray-900">
+                    ${price.unitAmount / 100}
+                  </span>{" "}
+                  / {price.interval}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul className="flex flex-col justify-center text-gray-700 text-sm divide-y divide-gray-200 text-center">
+                  <li className="py-2">Feature 1</li>
+                  <li className="py-2">Feature 2</li>
+                  <li className="py-2">Feature 3</li>
+                </ul>
+              </CardContent>
+              <CardFooter>
+                <AutoButton
+                  className={`w-full rounded-md py-2 px-4 font-medium transition-colors ${
+                    price.current
+                      ? "bg-gray-300 text-gray-700 cursor-not-allowed"
+                      : "bg-indigo-600 hover:bg-indigo-700 text-white"
+                  }`}
+                  action={
+                    updateEval
+                      ? api.updateSubscription
+                      : api.createCheckoutSession
+                  }
+                  variables={{
+                    priceId: price.id,
+                    subscriptionId: updateEval
+                      ? user.stripeSubscription.stripeId
+                      : undefined,
+                  }}
+                  onSuccess={({ data }) => {
+                    if (updateEval) navigate("/");
+                    else window.location.href = data;
+                  }}
+                  disabled={price.current}
+                >
+                  {price.current ? "Current" : "Select"}
+                </AutoButton>
+              </CardFooter>
             </div>
           );
-        }
       })}
-    </div>
+    </Card>
   );
 };
 
--- web/stripe-template/web/routes/_user.billing.tsx
+++ web/stripe-template/web/routes/_user.billing.tsx
@@ -1,3 +1,84 @@
-export default () => {
-  return <div>_user.billing</div>;
+import { useState } from "react";
+import { Route } from "./+types/_user.billing";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { AutoButton } from "@/components/auto";
+import { api } from "@/api";
+import { redirect } from "react-router";
+import ProductCard from "@/components/ProductCard";
+import { Switch } from "@/components/ui/switch";
+
+export type Product = {
+  name: string;
+  id: string;
+  prices: {
+    id: string;
+    unitAmount: number;
+    interval: string;
+    lookupKey: string;
+    current: boolean;
+  }[];
+};
+
+export const loader = async ({ context }: Route.LoaderArgs) => {
+  const userId = context.session?.get("user");
+
+  if (!userId) return redirect("/sign-in");
+
+  return {
+    products: await context.api.getProducts({ userId }),
+  };
+};
+
+export default ({ loaderData }: Route.ComponentProps) => {
+  const { products } = loaderData;
+  const [toggled, setToggled] = useState(false);
+
+  return (
+    <div className=" flex justify-center">
+      {products ? (
+        <div className="flex flex-col gap-6 w-full max-w-3xl p-6">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="font-bold text-2xl">Plans</h2>
+            <div className="flex flex-row items-center gap-2">
+              <span>{toggled ? "Yearly" : "Monthly"}</span>
+              <Switch
+                className="data-[state=checked]:bg-pink-400"
+                onCheckedChange={setToggled}
+              />
+            </div>
+          </div>
+          <section className="flex flex-row gap-1 justify-center">
+            {(products as Product[])
+              ?.sort((a, b) => a.prices[0].unitAmount - b.prices[0].unitAmount)
+              .map((product, i) => (
+                <ProductCard
+                  key={`product_${i}`}
+                  {...{ product, interval: toggled ? "year" : "month" }}
+                />
+              ))}
+          </section>
+        </div>
+      ) : (
+        <Card>
+          <CardHeader>
+            <CardTitle>No products yet created</CardTitle>
+          </CardHeader>
+          <CardContent>
+            No products found. If you wish to quickly create some products,
+            click the button below. Otherwise, make sure to add products in your
+            Stripe dashboard.
+          </CardContent>
+          <CardFooter>
+            <AutoButton action={api.addSampleProducts} />
+          </CardFooter>
+        </Card>
+      )}
+    </div>
+  );
 };

b9d6be743f11d697de03b5a7bf3e9a4c0ef070a6
Antoine Charette <antoineocharette@gmail.com>
2025-04-02 13:17:38 -0400

    Fixing an issue with the .gitignore

---
+++ shopify/product-quiz/product-quiz-custom-remix-spa/api/models/answer/actions/delete.ts
@@ -0,0 +1,21 @@
+import { deleteRecord, ActionOptions } from "gadget-server";
+import { preventCrossShopDataAccess } from "gadget-server/shopify";
+
+export const run: ActionRun = async ({ params, record, logger, api, connections }) => {
+  await preventCrossShopDataAccess(params, record);
+  await deleteRecord(record);
+};
+
+export const onSuccess: ActionOnSuccess = async ({ record, api }) => {
+  await api.internal.recommendedProduct.deleteMany({
+    filter: {
+      answerId: {
+        equals: record.id
+      }
+    }
+  });
+};
+
+export const options: ActionOptions = {
+  actionType: "delete",
+};
---
+++ shopify/product-quiz/product-quiz-custom-remix-spa/api/models/shopperSuggestion/schema.gadget.ts
@@ -0,0 +1,21 @@
+import type { GadgetModel } from "gadget-server";
+
+// This file describes the schema for the "shopperSuggestion" model, go to https://product-quiz-custom-remix-spa.gadget.app/edit to view/edit your model in Gadget
+// For more information on how to update this file http://docs.gadget.dev
+
+export const schema: GadgetModel = {
+  type: "gadget/model-schema/v1",
+  storageKey: "n9f1QAgNNUpx",
+  fields: {
+    product: {
+      type: "belongsTo",
+      parent: { model: "shopifyProduct" },
+      storageKey: "GmXGFkgT2Z4A",
+    },
+    quizResult: {
+      type: "belongsTo",
+      parent: { model: "quizResult" },
+      storageKey: "1_0pYmyXGw2e",
+    },
+  },
+};

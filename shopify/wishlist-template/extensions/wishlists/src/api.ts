59e7c50de93ce9024fc7b4936ca004d2d67b25be
Antoine Charette <antoineocharette@gmail.com>
2025-04-06 17:03:16 -0400

    Removing the old template from the lot

--- shopify/wishlist-template/extensions/wishlists/src/api.ts
+++
@@ -1,5 +0,0 @@
-// Change this to your application's API client
-import { Client } from "@gadget-client/wishlist-template";
-
-// CHANGE THIS TO THE ENVIRONMENT YOU"RE WORKING ON
-export const api = new Client({ environment: "development" });

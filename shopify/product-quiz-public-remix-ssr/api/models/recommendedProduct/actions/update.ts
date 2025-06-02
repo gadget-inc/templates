a5678e6240cc4e247b2a4c6269a0596746e8f4b2
Antoine Charette <antoineocharette@gmail.com>
2025-06-02 13:52:02 -0400

    Removed the old quiz application // added the new one

---
+++ shopify/product-quiz-public-remix-ssr/api/models/recommendedProduct/actions/update.ts
@@ -0,0 +1,12 @@
+import { applyParams, save, ActionOptions } from "gadget-server";
+import { preventCrossShopDataAccess } from "gadget-server/shopify";
+
+export const run: ActionRun = async ({ params, record, logger, api, connections }) => {
+  applyParams(params, record);
+  await preventCrossShopDataAccess(params, record);
+  await save(record);
+};
+
+export const options: ActionOptions = {
+  actionType: "update",
+};
--- shopify/product-quiz-template/web/routes/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CreateQuizPage } from "./CreateQuizPage";
-export { default as EditQuizPage } from "./EditQuizPage";
-export { default as HomePage } from "./HomePage";

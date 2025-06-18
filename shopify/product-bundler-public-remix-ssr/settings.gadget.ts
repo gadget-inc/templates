3db5ec56e1dc1be69a75952a953ecd2d152d90e0
Antoine Charette <antoineocharette@gmail.com>
2025-06-18 12:16:01 -0400

    Updating the framework version of the application

--- shopify/product-bundler-public-remix-ssr/settings.gadget.ts
+++ shopify/product-bundler-public-remix-ssr/settings.gadget.ts
@@ -2,7 +2,7 @@ import type { GadgetSettings } from "gadget-server";
 
 export const settings: GadgetSettings = {
   type: "gadget/settings/v1",
-  frameworkVersion: "v1.3.0",
+  frameworkVersion: "v1.4.0",
   plugins: {
     connections: {
       shopify: {
@@ -18,6 +18,9 @@ export const settings: GadgetSettings = {
           "write_cart_transforms",
           "write_products",
           "write_publications",
+          "read_cart_transforms",
+          "read_products",
+          "read_publications",
         ],
       },
     },

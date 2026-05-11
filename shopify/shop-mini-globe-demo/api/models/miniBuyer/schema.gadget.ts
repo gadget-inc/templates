88a926803ea1ff4de5487a742e1bf7d8d623707f
Julien <JulienDeWolfe@protonmail.com>
2026-05-11 14:20:14 -0400

    Update Gadget Framework version to v1.7.0

--- shopify/shop-mini-globe-demo/api/models/miniBuyer/schema.gadget.ts
+++ shopify/shop-mini-globe-demo/api/models/miniBuyer/schema.gadget.ts
@@ -1,6 +1,6 @@
 import type { GadgetModel } from "gadget-server";
 
-// This file describes the schema for the "miniBuyer" model, go to https://my-shop-mini.gadget.app/edit to view/edit your model in Gadget
+// This file describes the schema for the "miniBuyer" model, go to https://shop-mini-globe-demo.gadget.app/edit to view/edit your model in Gadget
 // For more information on how to update this file http://docs.gadget.dev
 
 export const schema: GadgetModel = {
